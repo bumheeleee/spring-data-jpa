409b784f515ccb9197be41594006414e6604a78f
이범희 <64251951+dlqjagml@users.noreply.github.com>
2022-10-21 16:29:24 +0900

    feat : data jpa repository생성 - team

--- src/main/java/study/datajpa/feature/member/repository/MemberRepository.java
+++ src/main/java/study/datajpa/feature/member/repository/MemberRepository.java
@@ -3,5 +3,6 @@ package study.datajpa.feature.member.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.feature.member.entity.Member;
 
+//@Repository를 생략이 가능
 public interface MemberRepository extends JpaRepository<Member, Long> {
 }
