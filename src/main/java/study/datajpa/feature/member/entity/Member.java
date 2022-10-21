29f6db238d464871f86380b25a6e556f7014edcd
이범희 <64251951+dlqjagml@users.noreply.github.com>
2022-10-21 15:24:03 +0900

    feat : 예제 도메인 모델과 동작확인 - 도메인 추가 완료 - 도메인 test 완료

--- src/main/java/study/datajpa/feature/member/entity/Member.java
+++ src/main/java/study/datajpa/feature/member/entity/Member.java
@@ -1,29 +1,44 @@
 package study.datajpa.feature.member.entity;
 
-import lombok.Getter;
+import lombok.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
-@Getter
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 public class Member {
-
     @Id
     @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
-
     private String username;
+    private int age;
 
-    protected Member() {
-    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String username) {
         this.username = username;
     }
 
-    public void changeUserid(Long id){
-        this.id = id;
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null){
+            changeTeam(team);
+        }else{
+            throw new NullPointerException("잘못된 입력값을 입력하였습니다.");
+        }
+    }
+
+    //-- 연관관계 편의 매소드 --//
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
