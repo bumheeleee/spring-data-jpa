0f14aebd48ef9fe43bd10f2189eb39410e0f5b67
dlqjagml <dlqjagml6447@naver.com>
2022-11-04 15:23:59 +0900

    feat : 페이징과 정렬

--- src/main/java/study/datajpa/feature/member/controller/MemberController.java
+++ src/main/java/study/datajpa/feature/member/controller/MemberController.java
@@ -1,9 +1,13 @@
 package study.datajpa.feature.member.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.feature.member.dto.MemberDto;
 import study.datajpa.feature.member.entity.Member;
 import study.datajpa.feature.member.repository.MemberRepository;
 
@@ -30,9 +34,24 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("members")
+    public Page<MemberDto> list(@PageableDefault(size = 5) Pageable  pageable){
+        Page<Member> page = memberRepository.findAll(pageable);
+
+        //엔티티로 반환하는 것은 좋지 않다! -> dto객체로 반환하기
+        Page<MemberDto> members = page.map(
+                member -> new MemberDto(member)
+        );
+        return members;
+    }
+
+    //test 데이터를 만들기 위해서
     @PostConstruct
     public void init(){
-        memberRepository.save(new Member("userA"));
+        for(int i = 0; i < 100; i ++){
+            memberRepository.save(new Member("user"+ i, i+1));
+        }
+
     }
 
 }
--- src/main/java/study/datajpa/feature/member/dto/MemberDto.java
+++ src/main/java/study/datajpa/feature/member/dto/MemberDto.java
@@ -1,6 +1,7 @@
 package study.datajpa.feature.member.dto;
 
 import lombok.Data;
+import study.datajpa.feature.member.entity.Member;
 
 @Data
 public class MemberDto {
@@ -8,9 +9,20 @@ public class MemberDto {
     private String username;
     private String teamName;
 
+    public MemberDto(Long id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+
     public MemberDto(Long id, String username, String teamName) {
         this.id = id;
         this.username = username;
         this.teamName = teamName;
+    }
+
+    //dto에서는 객체를 봐도 된다.
+    public MemberDto(Member member){
+        this.id = member.getId();
+        this.username = member.getUsername();
     }
 }
